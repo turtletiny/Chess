ef2c843f13e8d26450ff2b337aebeb48eb0379e0
Daniel <daniel.lukins1@gmail.com>
2026-05-25 19:44:21 +1000

    implemented clearCurrentPos (clears current piece position)

--- Piece.java
+++ Piece.java
@@ -1,11 +1,18 @@
 abstract class Piece {
 
     Colour colour;
+    int x,y; //coordinate position of piece
 
     Piece(Colour colour) {
         this.colour = colour;
     }
 
-    abstract void move(Board board, int x, int y);
+    void clearCurrentPos(Board board){
+        board.board[Math.abs(this.y-8)][this.x-1] = null; //changes current position to null
+
+    }
+
+
     abstract boolean isLegalMove();
-}
+
+    }
