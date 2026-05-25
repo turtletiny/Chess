c92d0e857458b2d54a58db38b159c99361b8803c
Daniel <daniel.lukins1@gmail.com>
2026-05-25 19:57:07 +1000

    implemented place piece method

--- Piece.java
+++ Piece.java
@@ -1,18 +1,21 @@
 abstract class Piece {
 
     Colour colour;
-    int x,y; //coordinate position of piece
+    int x, y; //coordinate position of piece
 
     Piece(Colour colour) {
         this.colour = colour;
     }
 
-    void clearCurrentPos(Board board){
-        board.board[Math.abs(this.y-8)][this.x-1] = null; //changes current position to null
-
+    void clearCurrentPos(Board board) {
+        board.board[Math.abs(this.y - 8)][this.x - 1] = null; //changes current position to null
     }
 
+    void placePiece(Board board, int x, int y) {
+        this.x = x;
+        this.y = y;
+        board.board[Math.abs(this.y - 8)][this.x - 1] = this;
+    }
 
     abstract boolean isLegalMove();
-
-    }
+}
