ed219ccaf155f5aabfc76e8749257dd6774d9655
Daniel <daniel.lukins1@gmail.com>
2026-05-26 20:28:12 +1000

    Added knight moves

--- Knight.java
+++ Knight.java
@@ -9,6 +9,9 @@ class Knight extends Piece {
     public void move(Board board) {}
 
     public boolean isLegalMove(int xDiff, int yDiff) {
+        if (!(Math.abs(xDiff) == 2 && Math.abs(yDiff) == 1) && !(Math.abs(xDiff) == 1 && Math.abs(yDiff) == 2)){
+            return false;
+        }
         return true;
     }
 
