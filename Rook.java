c3d67a2fa1fbb5692c91da3a018b0b3cc8a31b0f
Daniel <daniel.lukins1@gmail.com>
2026-05-29 14:37:17 +1000

    implemented collision check for rook

--- Rook.java
+++ Rook.java
@@ -12,15 +12,34 @@ class Rook extends Piece {
     }
 
     public boolean isLegalMove(Board board, int fromX, int fromY, int toX, int toY) {
-        if (!super.isLegalMove(board, fromX, fromY, toX, toY)){
+        if (!super.isLegalMove(board, fromX, fromY, toX, toY)) {
             return false;
         }
         int xDiff = toX - fromX;
         int yDiff = toY - fromY;
-        if (xDiff != 0 && yDiff != 0){
+        if (xDiff != 0 && yDiff != 0) {
             return false;
         }
         return true;
+    }
+
+    public boolean isBlocked(Board board, int fromX, int fromY, int toX, int toY) {
+        int xDiff = toX - fromX;
+        int yDiff = toY - fromY;
+        if (fromX == 0) { // then its a vertical move
+            for (int i = 1; i < Math.abs(toY - fromY); i++) {
+                if (board.pieceExists(fromX, fromY + i  * yDiff / Math.abs(yDiff))) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 1; i < Math.abs(toX - fromX); i++){
+                if (board.pieceExists(fromX + i * xDiff / Math.abs(xDiff), fromY)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public String toString() {
