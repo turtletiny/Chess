8653480d6542e4a42bc37d8d4e6d591bd0d93908
Daniel <daniel.lukins1@gmail.com>
2026-06-12 12:12:15 +1000

    changed move class to record

--- src/Move.java
+++ src/Move.java
@@ -1,35 +1,9 @@
 package src;
 
-class Move {
-    private final Point from, to;
-    private final Piece movedPiece, capturedPiece;
-
-    public Move(Point from, Point to, Piece movedPiece, Piece capturedPiece) {
-        this.from = from;
-        this.to = to;
-        this.movedPiece = movedPiece;
-        this.capturedPiece = capturedPiece;
-    }
-
-    public Point getFrom() {
-        return this.from;
-    }
-
-    public Point getTo() {
-        return this.to;
-    }
+public record Move(Point from, Point to, Piece movedPiece, Piece capturedPiece) {
 
     public Point getDir() {
         return new Point(Integer.compare(this.to.getX(), this.from.getX()),
                 Integer.compare(this.to.getY(), this.from.getY()));
     }
-
-    public Piece getMovedPiece() {
-        return this.movedPiece;
-    }
-
-    public Piece getCapturedPiece() {
-        return this.capturedPiece;
-    }
-
 }
