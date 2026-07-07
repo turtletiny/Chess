606e48c7f3a81111ad046b37628cf979c3ea3a3a
Daniel <daniel.lukins1@gmail.com>
2026-07-07 10:02:25 +1000

    implemented pawn move short form notation

--- src/Move.java
+++ src/Move.java
@@ -8,6 +8,10 @@ public record Move(Point from, Point to, Piece movedPiece, Piece capturedPiece, 
         this(null, null, null, null, colour, castleMove);
     }
 
+    public Move(Point from, Point to) {
+        this(from, to, null, null, null,null);
+    }
+
 
     public Point getDir() {
         return new Point(Integer.compare(this.to.getX(), this.from.getX()),
--- src/MoveNotation.java
+++ src/MoveNotation.java
@@ -1,41 +1,70 @@
 package src;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 class MoveNotation {
-    //could be hashmap char : piece 
-    HashSet<Character> validNotations = new HashSet<>();
+    public static HashMap<Character, Class<? extends Piece>> NOTATION_MAP = new HashMap<>();
 
     MoveNotation() {
-        this.validNotations.add('n');
-        this.validNotations.add('k');
-        this.validNotations.add('q');
-        this.validNotations.add('r');
-        this.validNotations.add('b');
+        MoveNotation.NOTATION_MAP.put('n', Knight.class);
+        MoveNotation.NOTATION_MAP.put('q', Queen.class);
+        MoveNotation.NOTATION_MAP.put('k', King.class);
+        MoveNotation.NOTATION_MAP.put('r', Rook.class);
+        MoveNotation.NOTATION_MAP.put('b', Bishop.class);
+        MoveNotation.NOTATION_MAP.put(' ', Pawn.class);
     }
-    void convertMove(String input) {
+
+    Move convertMove(String input, Board board) {
         String safeInput = input.trim().toLowerCase();
 
-        
         // Is a pawn move
         if (safeInput.length() == 2 &&
-                safeInput.charAt(0) >= 'a' && safeInput.charAt(0) <= 'z'
-                && safeInput.charAt(1) >= '1' && safeInput.charAt(1)<= '8') {
-                //dest = safeInput
-                //from = (if pawn exists that can move to dest)
-        } else if (safeInput.length() == 3 && 
-            this.isValidPieceNotation(safeInput.charAt(0))
-        ) {
-            //dest = charat1, charat2
-            //from = piece that can move to dest
+                safeInput.charAt(0) >= 'a' && safeInput.charAt(0) <= 'h'
+                && safeInput.charAt(1) >= '1' && safeInput.charAt(1) <= '8') {
+            Point toSquare = new Point(safeInput.charAt(0) - 96, safeInput.charAt(1) - 48);
+            Point initialPoint = MoveNotation.pieceExists(toSquare, ' ', board); //blank char == pawn notation
+            if (initialPoint == null) {
+                return null;
+            } else {
+                return new Move(initialPoint, toSquare);
+            }
+
+
+
+
+
+        } else if (safeInput.length() == 3 &&
+                this.isValidPieceNotation(safeInput.charAt(0))) {
+            // dest = charat1, charat2
+            // from = piece that can move to dest
         }
 
     }
 
-
     public boolean isValidPieceNotation(char c) {
-            return this.validNotations.contains(c);
+        return MoveNotation.NOTATION_MAP.containsKey(c);
+    }
+
+    public static Point pieceExists(Point toSquare, char notation, Board board) {
+        Class<? extends Piece> targetClass = NOTATION_MAP.get(notation);
+        int validCount = 0;
+        Point validPoint = null;
+        for (Piece p : board.board) {
+            if (p != null &&
+                    targetClass.isInstance(p) &&
+                    p.isLegalMove(board, p.getPoint(), toSquare)) {
+                validCount++;
+                validPoint = p.getPoint();
+            }
         }
+        if (validCount == 1) {
+            return validPoint;
+        } else {
+            return null;
+        }
+    }
 }
 
 // ne4
